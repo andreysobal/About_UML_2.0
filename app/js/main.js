7b7ec65b6b44ce4c06e257cc474a8cf7bf731fe1
andreysobal <say6287@gmail.com>
2017-08-13 21:28:55 +0300

    Added toggle menu for small devices

--- app/js/main.js
+++ app/js/main.js
@@ -36,7 +36,6 @@ $(document).ready(function() {
 
 	//to-top button
 	$(window).scroll(function () {
-		console.log('scroll', $(this).scrollTop());
     if ($(this).scrollTop() > 200) {
 			$('#extra3').fadeIn();
 		} else {
@@ -44,9 +43,30 @@ $(document).ready(function() {
 		}//end if
   });//end scroll
 	
+	//to-top button
+
   $('#extra3').click(function () {
     $('body, html').animate({
       scrollTop: 0
     }, 500);
   });//end click
+
+  //animated menu for small devices
+
+  $(document).on('click', function(evt) {															//закрыть всплывающее меню авторизации по клику на любом свободном месте
+  	if ($(window).innerWidth() <= 902){
+  		if ($(evt.target).closest(".menu").length && $(".menu").css("left") == "-230px") {
+				$('.menu').stop().animate({
+					left: 0
+				},1000);
+			};//end if
+			if (!$(evt.target).closest(".menu").length && $(".menu").css("left") == "0px") {
+				$('.menu').stop().animate({
+					left: "-230px"
+				},1000);
+			};//end if
+  	};//end if
+  	evt.stopPropagation();
+	});//end click 
+
 });//end ready
